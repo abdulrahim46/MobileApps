60357d9a4ef2249d77646c447a68ef9a4b4f7377
abdulrahim46 <abdul7716@gmail.com>
2021-10-11 22:18:08 +0530

    Add constants error messages

--- MobileApps/Common/Constants.swift
+++ MobileApps/Common/Constants.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+// MARK:- Common Error messages
+
+struct ErrorMessage {
+    static let kNoInternetTitle = "No Internet Connectivity!"
+    static let kConnectivityError = "Please check your internet connectivity and try again later."
+    static let kSomethingWentWrong = "Something went wrong. Please try again later."
+    static let kSuccessMessage = "Success"
+    static let kWaitForPost = "Please wait..."
+}
