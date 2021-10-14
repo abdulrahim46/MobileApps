8efd213a0c90f0c377ec99dcae13ba72a605adc7
abdulrahim46 <abdul7716@gmail.com>
2021-10-14 07:30:41 +0530

    Add VCs for all & favourite

---
+++ MobileApps/View/AllMobileVC.swift
@@ -0,0 +1,18 @@
+//
+//  AllMobileVC.swift
+//  MobileApps
+//
+//  Created by Abdul Rahim on 14/10/21.
+//
+
+import UIKit
+
+class AllMobileVC: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .brown
+    }
+    
+    
+}
---
+++ MobileApps/View/FavouriteMobileVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavouriteMobileVC.swift
+//  MobileApps
+//
+//  Created by Abdul Rahim on 14/10/21.
+//
+
+import UIKit
+
+class FavouriteMobileVC: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    
+}
