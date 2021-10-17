119f501150b5c9f03c2858450d2a363a2428a1fa
abdulrahim46 <abdul7716@gmail.com>
2021-10-17 21:31:02 +0530

    Setup UI in code for detail view

--- MobileApps/View/Cell/DetailCollectionViewCell.swift
+++ MobileApps/View/Cell/DetailCollectionViewCell.swift
@@ -9,4 +9,41 @@ import UIKit
 
 class DetailCollectionViewCell: UICollectionViewCell {
     
+    
+    fileprivate let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
+    func configure(images: MobileDetail) {
+        if var imageData = images.image {
+            if imageData.hasPrefix("www") {
+                imageData = "http://" + imageData
+            }
+            imageView.sd_setImage(with: URL(string: imageData),
+                                  placeholderImage: UIImage(named: "placeholder"),
+                                  options: .continueInBackground,
+                                  completed: nil)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
 }
--- MobileApps/View/DetailViewController.swift
+++ MobileApps/View/DetailViewController.swift
@@ -17,33 +17,116 @@ class DetailViewController: UIViewController {
     // MARK: - Properties & View
     let viewModel = DetailViewModel()
     var mobile: Mobile?
-
+    
+    fileprivate let collectionV: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(DetailCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return cv
+    }()
+    
+    fileprivate let descriptionLb: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    fileprivate let overView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).withAlphaComponent(0.6)
+        return view
+    }()
+    
+    fileprivate let priceLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.sizeToFit()
+        label.font = .boldSystemFont(ofSize: 12)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    fileprivate let ratingLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.sizeToFit()
+        label.font = .boldSystemFont(ofSize: 12)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCollectionView()
+        setupLabel()
         setupView()
         fetchDetails()
+        collectionView.isHidden = true
+        descriptionLabel.isHidden = true
     }
     
     
     func setupCollectionView() {
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionV.dataSource = self
+        collectionV.delegate = self
+        collectionV.register(DetailCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionV.backgroundColor = .clear
+        [collectionV, overView, priceLabel, ratingLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+
+        NSLayoutConstraint.activate([
+            collectionV.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionV.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionV.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionV.heightAnchor.constraint(equalToConstant: view.bounds.height * 0.35),
+            
+            overView.topAnchor.constraint(equalTo: view.topAnchor),
+            overView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            overView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            overView.heightAnchor.constraint(equalToConstant: 30),
+            
+            priceLabel.topAnchor.constraint(equalTo: overView.topAnchor),
+            priceLabel.trailingAnchor.constraint(equalTo: overView.trailingAnchor, constant: -10),
+            priceLabel.heightAnchor.constraint(equalToConstant: 30),
+            
+            ratingLabel.topAnchor.constraint(equalTo: overView.topAnchor),
+            ratingLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            ratingLabel.heightAnchor.constraint(equalToConstant: 30),
+        ])
+        
+    }
+    
+    func setupLabel() {
+        view.addSubview(descriptionLb)
+        NSLayoutConstraint.activate([
+            descriptionLb.topAnchor.constraint(equalTo: collectionV.bottomAnchor, constant: 10),
+            descriptionLb.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            descriptionLb.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            //descriptionLb.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 10)
+        ])
     }
     
     func setupView() {
         title = mobile?.title
         descriptionLabel.text = mobile?.description
+        descriptionLb.text = mobile?.description
+        priceLabel.text = "Price: $\(mobile?.price ?? 0)"
+        ratingLabel.text = "Rating: \(mobile?.rating ?? 0)"
     }
     
     private func fetchDetails() {
         let id:String = String(mobile?.id ?? 0)
         viewModel.getAllImages(id: id, completion: { [weak self] res, err  in
             DispatchQueue.main.async {
-                self?.collectionView.reloadData()
+                self?.collectionV.reloadData()
             }
         })
     }
@@ -61,10 +144,10 @@ extension DetailViewController: UICollectionViewDelegate, UICollectionViewDataSo
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .red
-        //cell.configure()
-        // viewModel.configure(item: cell, for: indexPath)
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? DetailCollectionViewCell else {
+            fatalError("Could not dequeue DetailCollectionviewCell")
+        }
+        cell.configure(images: viewModel.details[indexPath.row])
         
         return cell
     }
